4d5ccd2a6f08f1c56f687e76700084e01e53abcf
Dhruv Pawar <dhruvpawar229@gmail.com>
2025-06-27 15:42:48 +0530

    finished cart service

--- cart-service/src/routes/cart_route.js
+++ cart-service/src/routes/cart_route.js
@@ -5,15 +5,17 @@ const Cart = require('../models/Cart')
 const authenticateToken = require('../middleware/authenticateToken')
 
 //get current users cart get
-router.get('/',authenticateToken, async (req,res) => {
-    try{
-        const cart = await Cart.find({userId:req.user.id})
-        res.status(200).json(cart)
+router.get('/', authenticateToken, async (req, res) => {
+    try {
+        const cart = await Cart.findOne({ userId: req.user.id });
+        if (!cart || cart.products.length === 0) {
+            return res.status(404).json({ message: "Cart is empty" });
+        }
+        res.status(200).json(cart);
+    } catch (error) {
+        res.status(500).json({ message: "Error getting cart" });
     }
-    catch(error){
-        res.status(500).json({message:"Error getting Order"})
-    }
-} )
+})
 //add products to cart post
 router.post('/add', authenticateToken,async (req,res) => {
     try {
@@ -47,9 +49,35 @@ router.post('/add', authenticateToken,async (req,res) => {
 })
 
 //remove singular product from cart 
-
+router.delete('/delete/:id',authenticateToken, async (req,res)=>{
+    try {
+        const cart = await Cart.findOne({userId:req.user.id});
+        if(!cart)
+        {
+            return res.status(404).json({message:"Cart not found for user"})
+        }
+        cart.products = cart.products.filter(
+            (item)=>item.productId != req.params.id
+        )
+        await cart.save();
+        res.status(200).json({message:"product removed",cart})
+    } catch (error) {
+        res.status(500).json({ message: "Error removing product", error });
+    }
+})
 //remove all products from cart
+router.delete('/deleteall', authenticateToken, async (req,res)=>{
+try {
+    const cart = await Cart.findOneAndDelete({userId:req.user.id});
+    if(!cart){
+        return res.status(404).json({message:"Cart not found"});
+    }
+    res.status(200).json({message:"cart deleted"})
+} catch (error) {
+            res.status(500).json({ message: "Error removing Cart", error });
 
+}
+})
 
 
 module.exports=router;
